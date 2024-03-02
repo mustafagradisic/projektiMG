648763a77659f7436aac40d44bf5c49ff5a034c0
mustafagradisic <gradisicmustafa@gmail.com>
2024-03-02 14:35:19 +0100

    innit

--- projekticysecor/script.js
+++ projekticysecor/script.js
@@ -1,21 +1,24 @@
 
-let novcanice = 34 //25 novcaniac
-let vrijednost =10 // vrijsdnost jedne
+let novcanice = [1,2,3,4,5,6,7,8,9,10,11,12,13]
+let brojpara= novcanice.length
+let brojac= 0
 
 
-//brojac
+while(brojac< brojpara){
+console.log('-----------')
+console.log('uslov prosao')
+console.log(`novcanica na poziciji [${brojac}]: ${novcanice[brojac]} `  )
+console.log('-----------')
+ brojac++
+}
+console.log('-----------')
+
+if (brojac < brojpara){
 
 
-
-//izbrojane 
-let izbrojane=0
-
-/*broji novcanicxe 
-sve dok je brojkac manji od broja nocanica*/
-while(izbrojane<novcanice) {
+    console.log('brojac je manji')
+}else{
+    console.log('brojac nije manji ')
  
-   
-    izbrojane++
-    }
-
-    console.log(izbrojane * vrijednost)
+}
+console.log('brojac '  + brojac)
