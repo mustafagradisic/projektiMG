aa97ef9c82821f92f25171ccfd714ac50b73b199
mustafagradisic <gradisicmustafa@gmail.com>
2024-03-04 16:13:33 +0100

    innit

--- projekticysecor/script.js
+++ projekticysecor/script.js
@@ -1,21 +1,25 @@
 let nekretnine = {
 
-stan: 7000,
-kuca: 80000,
-plac: 3000
+    stan: 9000,
+    kuca: 80000,
+    plac: 3000
+    }
+
+    racunanje_mjesecne_rate(6, 'kuca')
+
+
+racunanje_mjesecne_rate(15, 'plac')
+
+racunanje_mjesecne_rate(21, 'stan')
+function racunanje_mjesecne_rate(godine, vrsta_nekretnine){
+ let mjeseci = godine*12
+ let rata= nekretnine[vrsta_nekretnine] / mjeseci
+    rata= rata.toFixed(2)
+
+    console.log('------------')
+   
+    console.log(`mjesecna rata za  ${vrsta_nekretnine} je ${rata} eura na ${godine} godina `)
+
+ 
+
 }
-
-let godine = 15
-let mjeseci = godine *12
-let rata = nekretnine.stan / mjeseci
-
-rata = rata.toFixed(2)
-
-console.log(mjeseci)
-
-console.log(rata)
-
-console.log(`mjesecna rata za stan je ${rata} eura.
-mjesecna rata za kucu ${nekretnine.kuca / mjeseci} eura
-mjesecna rata za plac ${nekretnine.plac / mjeseci} eura`)
-
