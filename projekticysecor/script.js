19d91eec85b709f3d8762f3fd57e13c0057da16e
mustafagradisic <gradisicmustafa@gmail.com>
2024-03-05 23:10:30 +0100

    innit

--- projekticysecor/script.js
+++ projekticysecor/script.js
@@ -1,28 +1,14 @@
-/*let brend = document.getElementsByTagName('span'   )
-
-for(let auto of brend){
-console.log(auto.innerText)
-
-}
-console.log('----------')
-
-let modeli = document.getElementsByClassName('automobil-model')
-for(let   model of modeli){
-    console.log(model.innerText)}
-
-let najbolji = document.getElementById('recenica')
-console.log(najbolji.innerText) 
-
-console.log('----------')
-*/
-let brend = document.querySelectorAll('span')
-for(let auto of brend){
-    console.log(auto.innerText)}
-    console.log('----------')
-let modeli = document.querySelectorAll( '.automobil-model' )
-for(let model of modeli){
-    console.log(model.innerText)}
-    console.log('----------')
 
     let recenica = document.querySelector('#recenica');
-    console.log(recenica.innerText) 
+ recenica.remove()
+
+
+
+ let novi_el = document.createElement('div')
+ novi_el.classList = ' novi element'   
+ novi_el.innerHTML = ' <hr> <br> Pozdrav'
+
+
+
+let body = document.querySelector('body')
+body.appendChild(novi_el) 
